541ba356c8d60693d155f7508da01f8c39a6de69
Niranjan Dorage <108044574+Niranjan-Dorage@users.noreply.github.com>
2022-12-28 20:16:57 +0530

    .
    .

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let temp = 0;
 let temp2 = 0;
 let p = 0;
-let q=72;
+let q = 60;
+let h = 1;
+let score = 0;
 document.onkeydown = function (e) {
     console.log("key code is:", e.keyCode)
     if (e.keyCode == 32) {
@@ -15,7 +17,7 @@ document.onkeydown = function (e) {
         up();
     }
 }
- 
+
 
 function fire() {
     // if (temp == 0) {
@@ -57,22 +59,94 @@ function fire() {
     // setTimeout(( bullet.classList.remove('animatebullet')),1000);
     // bullet.classList.remove('animatebullet');
 }
-let m=20;
+let m = 20;
 function down() {
-  if(p<420)
-    {  p = p + m;
-    q=q+m;
-    document.querySelector('.tank').style.top = p + "px";
-    document.querySelector('.bullet').style.top = q + "px";}
+    if (p < 420) {
+        p = p + m;
+        q = q + m;
+        document.querySelector('.tank').style.top = p + "px";
+        document.querySelector('.bullet').style.top = q + "px";
+    }
 
 
 }
 function up() {
-    if(p>-10)
-   { p = p -m;
-    q=q-m;
+    if (p > -10) {
+        p = p - m;
+        q = q - m;
 
-    document.querySelector('.tank').style.top = p + "px";
-    document.querySelector('.bullet').style.top = q + "px";}
+        document.querySelector('.tank').style.top = p + "px";
+        document.querySelector('.bullet').style.top = q + "px";
+    }
 
-} 
+}
+setInterval(() => {
+    ufo = document.querySelector('.tank');
+    gameover = document.querySelector('.gameover');
+    obstacle = document.querySelector('.obstacle');
+    bullet = document.querySelector('.bullet');
+
+    dx = parseInt(window.getComputedStyle(bullet, null).getPropertyValue('left'));
+    px = parseInt(window.getComputedStyle(ufo, null).getPropertyValue('left'));
+    py = parseInt(window.getComputedStyle(ufo, null).getPropertyValue('top'));
+
+
+    dy = parseInt(window.getComputedStyle(bullet, null).getPropertyValue('top'));
+
+    ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
+
+    oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('top'));
+
+    offsetX = Math.abs(dx - ox);
+    offsetY = Math.abs(dy - oy);
+    offsetXX = Math.abs(px - ox);
+    offsetYY = Math.abs(py - oy);
+    console.log(offsetXX, offsetYY);
+
+
+    if (obstacle.style.visibility != 'hidden') {
+         
+   
+
+        if (offsetXX < 93 && offsetYY < 52) {
+            gameover.style.visibility = 'visible';
+            obstacle.style.visibility='hidden';
+            bullet.style.visibility='hidden';
+
+        }
+
+
+
+  else if (offsetX < 100 && offsetY < 120) {
+            // obstacle.style.animationname="hello";     
+            score++;
+            document.querySelector('.score').innerHTML = 'Score :- ' + score;
+            obstacle.style.visibility = 'hidden';
+            setTimeout(() => {
+                obstacle.style.visibility = 'visible';
+
+            }, 4000);
+
+            //    obstacle.classList.remove('animateobstacle');  
+
+            // setTimeout(() => {
+            //h++;
+            //     obstacle.style.visibility='visible';
+            //     obstacle.style.animationIterationCount=h;     
+
+            // }, 3000)
+
+
+            // gameover.style.visibility = 'visible';
+            // obstacle.classList.remove('obstacleAni');
+        }
+
+    }
+
+}, 100);
+function resetanimation() {
+    var el = document.querySelector('.obstacle');
+    el.style.animation = 'none';
+    el.offsetHeight; /* trigger reflow */
+    el.style.animation = null;
+}
